d8b718be6eeb2ebb18e65d2ee9d8cf8b0565df21
ScriptLineStudios <scriptlinestudios@protonmail.com>
2022-01-27 21:42:25 +0200

    New changes

--- website/app.py
+++ website/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
+import secrets
+import random
+import time
 
 app = Flask(__name__)
 db_name = 'sockmarket.db'
@@ -8,13 +11,27 @@ db = SQLAlchemy(app)
 
 class Leaderboard(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True, nullable=False)
+    name = db.Column(db.String(20), nullable=False)
     data = db.Column(db.String(10000))
-    api_key = db.Column(db.String(30), unique=True)
+    api_key = db.Column(db.String(30))
     scores = db.Column(db.String(1000))
 
     def __repr__(self):
         return f"Leaderboard('{self.name}', '{self.data}', '{self.api_key}', '{self.scores}')"
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/new_leaderboard', methods=['POST'])
+def create_new_leaderboard():
+    if request.method == 'POST':
+        name = str(request.form["name"])
+        api_key = secrets.token_hex(32)
+        db.session.add(Leaderboard(id=random.randrange(-100000, 100000), name=name, data="", api_key=str(api_key), scores=""))
+        db.session.commit()
+        return f"Your api key is {api_key}"
+
 
 @app.route('/add', methods=["POST"])
 def landing_page():
